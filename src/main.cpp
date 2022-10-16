d67ad70988b3b35ed30315db32e115da0f385d96
Leontyev Alexandr <leontyev.alexandr.a@gmail.com>
2022-10-16 16:02:26 +0300

    Update example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
-#include "log/log.h"
+// #include "log/log.h"
 
-int main(int, char**) {
-    printf("Hello, world!\n");
-    logMe();
+const float FLOAT_EPSILON = 0.000000001;
+
+void main(int, char**) {
+    int a = 10, b = 11;
+    if(a = b) {
+        // do nothing
+    }
+    if(a >= b) {
+        printf("a >= b");
+    } else {
+        printf("a < b");
+    }
+
+    float fa = 0.1, fb = 0.2;
+    if(fa - fb >= FLOAT_EPSILON) {
+        printf("fa >= fb");
+    } else {
+        printf("fa < fb");
+    }
 }
