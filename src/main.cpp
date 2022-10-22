3a538f6a00f6ec06a6d7075334d465e0ffa590fb
Leontyev Alexandr <leontyev.alexandr.a@gmail.com>
2022-10-22 16:07:19 +0300

    Simplify example code

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,6 @@
 const float FLOAT_EPSILON = 0.000000001;
 
 int main(int, char**) {
-    int a = 10, b = 11;
-    if(a = b) {
-        // do nothing
-    }
-    if(a >= b) {
-        printf("a >= b");
-    } else {
-        printf("a < b");
-    }
-
-    float fa = 0.1, fb = 0.2;
-    if(fa - fb >= FLOAT_EPSILON) {
-        printf("fa >= fb");
-    } else {
-        printf("fa < fb");
-    }
-
+    printf("Hello world!");
     return 0;
 }
