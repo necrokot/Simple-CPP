87bf72fb4adb07f6b10554559e1ca78317a3042c
Ekaterina Ushakova <katya972265@gmail.com>
2022-11-04 00:51:46 +0300

    add guess game

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <stdio.h>
 // #include "log/log.h"
 
-const float FLOAT_EPSILON = 0.000000001;
+int main(int argc, char** argv) {
+    printf("pick a number\n");
 
-int main(int, char**) {
-    printf("Hello world!");
+    char input;
+    int leftBorder, rightBorder, guess;
+
+    printf("enter a value for the left border\n");
+    scanf("%i", &leftBorder);
+    printf("enter a value for the right border\n");
+    scanf("%i", &rightBorder);
+
+    while (true)
+    {
+        guess = leftBorder + (rightBorder - leftBorder)/2;
+        printf("%i - is it your number?\n", guess);
+        scanf("%1s", &input);
+        // getchar();
+
+        switch (input)
+        {
+        case '=':
+            printf("done\n");
+            return 0;
+        case '<':
+            rightBorder = guess;
+            break;
+
+        case '>':
+            leftBorder = guess;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
     return 0;
 }
