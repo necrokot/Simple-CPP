c6a3a2dbff3ae81b7822214830857ab3094a7b4d
Ekaterina Ushakova <katya972265@gmail.com>
2022-11-13 16:48:43 +0300

    random points generate

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,33 @@ struct Point {
     int y;
 };
 
+Point generatePoint () {
+    int x = rand()%201-100;
+    int y = rand()%201-100;
+    return {x, y};
+}
+
+void printPoint (Point p) {
+    printf("{%i, %i}\n", p.x, p.y);
+}
+
 int main(int, char**) {
-    Point a = {0, 0}, b = {10, 100};
 
-    char *dynAdress = (char *)malloc(2*N);
-    delete dynAdress;
+    unsigned int numberOfPoints;
+    printf("enter the number of points\n");
+    scanf("%i", &numberOfPoints);
 
+    Point *points = (Point *)malloc(sizeof(Point)*numberOfPoints);
+
+    for (unsigned int i = 0; i < numberOfPoints; i++)
+    {
+        points[i] = generatePoint();
+    }
+
+    for (unsigned int i = 0; i < numberOfPoints; i++)
+    {
+        printPoint(points[i]);
+    }
+    
     return 0;
 }
